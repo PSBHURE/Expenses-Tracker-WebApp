a16a2d1f67625c262432dfa3635a9e778dede365
PRATIK BHURE <147384272+PSBHURE@users.noreply.github.com>
2025-08-25 23:48:37 +0530

    Update S3.tf

--- Terraform/bootstrap/S3.tf
+++ Terraform/bootstrap/S3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "remote_s3" {
   bucket = "git-docker-terraform-intigration-state-bucket"
+  force_destroy = true
 
   tags = {
     Name = "git-docker-terraform-intigration-state-bucket"
